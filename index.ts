b4f8eda5b9843e68d1b291a40786be455879d458
Th1nkK1D <witheep@gmail.com>
2021-03-17 22:45:14 +0700

    Add staging banner to index file

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import Footer from './components/footer';
 import SocialSharer from './components/social-sharer';
 import NavigationBar from './components/navigation-bar';
+import StagingBanner from './components/staging-banner';
 
-export { Footer, SocialSharer, NavigationBar };
+export { Footer, SocialSharer, NavigationBar, StagingBanner };
