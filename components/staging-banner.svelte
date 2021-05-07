f9ff0c71958f73b8f8457b0927e7a6c373298d05
Th1nkK1D <witheep@gmail.com>
2021-05-07 17:38:50 +0700

    Migrate staging banner to svelte

---
+++ components/staging-banner.svelte
@@ -0,0 +1,40 @@
+<svelte:options tag="staging-banner" />
+
+<script lang="ts">
+  let productionUrl: string = '';
+</script>
+
+<div class="bg-gray-600 text-white text-xs font-body px-4 py-2">
+  <div class="inline-flex">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="w-3 my-auto"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="3"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path
+        d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"
+      />
+      <line x1="12" y1="9" x2="12" y2="13" />
+      <line x1="12" y1="17" x2="12.01" y2="17" />
+    </svg>
+  </div>
+  <strong>คำเตือน</strong>
+  เว็บไซต์นี้มีจุดประสงค์ในการทดสอบภายในทีม ELECT เท่านั้น อาจมีข้อมูลและการทำงานที่ไม่ถูกต้อง
+  ทางทีมงานจะไม่รับผิดชอบข้อผิดพลาดใด ๆ ที่เกิดขึ้น
+  {#if productionUrl}
+    ท่านสามารถชมชิ้นงานจริงได้
+    <a
+      href="${productionUrl}"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="underline"
+    >
+      ที่นี่
+    </a>
+  {/if}
+</div>
---
+++ stories/components/staging-banner.stories.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  import StagingBanner from '../../components/staging-banner.svelte';
+</script>
+
+<Meta
+  title="Components/staging-banner"
+  component={StagingBanner}
+  argTypes={{
+    productionUrl: { control: 'text' },
+  }}
+/>
+
+<Template let:args>
+  <StagingBanner {...args} />
+</Template>
+
+<Story
+  name="Primary"
+  args={{
+    productionUrl: '',
+  }}
+/>
