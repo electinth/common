a41ff359266d3ce0323b522c3ee8eba41a8c9805
Th1nkK1D <witheep@gmail.com>
2021-05-07 17:30:34 +0700

    Add storybook for svelte

---
+++ stories/components/social-sharer.stories.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+  import SocialSharer from '../../components/social-sharer.svelte';
+</script>
+
+<Meta
+  title="Components/social-sharer"
+  component={SocialSharer}
+  argTypes={{
+    url: { control: 'text' },
+    light: { control: 'boolean' },
+    hideLabel: { control: 'boolean' },
+    vertical: { control: 'boolean' },
+  }}
+/>
+
+<Template let:args>
+  <SocialSharer {...args} />
+</Template>
+
+<Story
+  name="Primary"
+  args={{
+    url: 'https://elect.in.th',
+  }}
+/>
+
+<Story
+  name="Vertical"
+  args={{
+    url: 'https://elect.in.th',
+    vertical: true,
+  }}
+/>
