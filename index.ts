2c192780a0e7597d70136f18c1aec94b29af3532
Th1nkK1D <witheep@gmail.com>
2021-05-17 11:28:10 +0700

    Remove unused index.ts

--- index.ts
+++
@@ -1,6 +0,0 @@
-import Footer from './components/footer';
-import SocialSharer from './components/social-sharer';
-import NavigationBar from './components/navigation-bar';
-import StagingBanner from './components/staging-banner';
-
-export { Footer, SocialSharer, NavigationBar, StagingBanner };
